5857035afae5b2653ae776e24a01d5d32c87814f
Giulia Cantini <vermouth_95@hotmail.it>
2017-06-04 16:52:43 +0200

    Add AP bottom sheet

--- app/src/main/java/io/github/giulic3/apmap/MainActivity.java
+++ app/src/main/java/io/github/giulic3/apmap/MainActivity.java
@@ -5,17 +5,24 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
@@ -23,8 +30,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /*
@@ -33,30 +43,47 @@ import com.google.android.gms.maps.model.MarkerOptions;
 * */
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener,
-        ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        ConnectionCallbacks, OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private static final LatLng BOLOGNA_POINT = new LatLng(44.496781, 11.356387);
-    private LatLng currentLocation = new LatLng(0,0);
-    private LocationManager locationManager;
 
-    private static final long MIN_TIME = 10;
-    private static final float MIN_DISTANCE = 100;
-
-    private double currentLatitude;
-    private double currentLongitude;
 
     private Location mLastLocation;
     private double mLastLatitude;
     private double mLastLongitude;
+    private LatLng mLastPosition;
 
-    private Geocoder geocoder;
+
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest locationRequest;
+
+
+    private BottomSheetBehavior mBottomSheetBehavior1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("DEBUG", "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getLocation();
+        if (mMap == null) {
+            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
+
+    }
+
+
+    private void getLocation(){
+
+        Log.d("DEBUG", "getLocation()");
+
+
 
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -67,154 +94,190 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .build();
         }
 
-
-        initializeMap();
-
     }
 
-
     protected void onStart() {
-        mGoogleApiClient.connect();
+
+        Log.d("DEBUG", "onStart()");
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.connect();
         super.onStart();
     }
 
     protected void onStop() {
-        mGoogleApiClient.disconnect();
+
+        Log.d("DEBUG", "onStop()");
+
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.disconnect();
         super.onStop();
     }
 
 
-
-    // se il permesso è già stato accettato, la preferenza è salvata e posso andare subito alla
-    // localizzazione
     @Override
     protected void onResume() {
+
+        Log.d("DEBUG", "onResume()");
+
         super.onResume();
 
         /* ad ogni ricaricamento dell'activity, controllo le permission (che sono modificabili dai settings)*/
+        /*
         if (checkLocationPermission()) {
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
 
-                if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER))
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                if (mGoogleApiClient != null)
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
 
             }
         }
+        */
     }
 
+
+    /* starts as callback when the phone connects to a GoogleApiClient */
     @Override
     public void onConnected(Bundle connectionHint) {
 
-        if (checkLocationPermission()) {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            if (mLastLocation != null) {
-                mLastLatitude = mLastLocation.getLatitude();
-                mLastLongitude = mLastLocation.getLongitude();
-                //TODO: FATTO UN PO' A CASO
-                LatLng mLastPosition = new LatLng(mLastLatitude,mLastLongitude);
-                mMap.addMarker(new MarkerOptions().position(mLastPosition).title("Posizione attuale"));
+        Log.d("DEBUG", "onConnected()");
 
-            }
+        /* a locationRequest object must be prepared before asking for permission */
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        /* these determines how often onLocationChanged() will be called,
+         time is in ms, aka 30 seconds */ //TODO: set time according to common sense
+        locationRequest.setInterval(30000);
+        /* 30 seconds*/
+        locationRequest.setFastestInterval(30000);
+
+
+        if (checkLocationPermission()) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+
         }
     }
 
+    /* starts as callback when connection between GoogleApi and phone is suspended */
     @Override
     public void onConnectionSuspended(int i) {
 
+        Log.d("DEBUG", "onConnectionSuspended()");
     }
 
+    /* starts as callback when phone can't connect to GoogleApiServices, for example, no GoogleServices?*/
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
-    }
+        Log.d("DEBUG", "onConnectionFailed()");
 
-    private void initializeMap() {
-
-        geocoder = new Geocoder(this);
-
-        // Get instance of LocationManager system service
-        locationManager=(LocationManager) getSystemService(LOCATION_SERVICE);
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-
-        if (mMap == null) {
-            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this); // this refers to onMapReady? why?
-        }
     }
 
     // TODO: fill in mettendo dei setting onesti
     // used for map settings (when displayed at first)
     // es. voglio mostrare la mappa con la mia locazione
+
+    /* this method contains all the commands to customize the map */
     private void setUpMap() {
 
+        Log.d("DEBUG", "setUpMap()");
+
+        /*
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(BOLOGNA_POINT)
                 .zoom(15)
-                .bearing(90)
-                .tilt(30)
+                .bearing(0)
+                .tilt(0)
                 .build();
 
         // CameraUpdate != CameraPosition
         CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
         mMap.moveCamera(cameraUpdate);
+        */
 
-        mMap.addMarker(new MarkerOptions().position(BOLOGNA_POINT).title("Dipartimento Di Informatica DISI"));
 
 
     }
 
+    /* callback interface for when the map is ready to be used */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        Log.d("DEBUG", "onMapReady()");
         mMap = googleMap;
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                mMap.setMyLocationEnabled(true);
+            }
+            else {
+                checkLocationPermission();
+            }
+        }
+        else {
+            mMap.setMyLocationEnabled(true);
+        }
         setUpMap();
     }
 
 
-
-
+    /* this method starts as callback when the LocationListener listens for a change in the current location */
     @Override
     public void onLocationChanged(Location location) {
 
-        Toast t= Toast.makeText(this,"New GPS Sample Available", Toast.LENGTH_SHORT);
-        t.show();
 
-        // aggiungo un marker ogni volta che cambio location (da emulatore posso "mandarla" altr.
-        // non sarebbe fattibile)
+            Log.d("DEBUG", "onLocationChanged()");
+            Toast.makeText(this, "location :"+location.getLatitude()+" , "+location.getLongitude(), Toast.LENGTH_LONG).show();
+            mLastLocation = location;
+
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("CurrentPosition")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))); //marker del colore che si vuole
+
+            // move camera to current position and focus
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
 
-        currentLatitude = location.getLatitude();
-        currentLongitude = location.getLongitude();
-        //how to? change locationcurrentLocation =
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(currentLocation)
-                .zoom(15)
-                .bearing(90)
-                .tilt(30)
-                .build();
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    if (marker.getTitle().equals("CurrentPosition")) {
 
-        // CameraUpdate != CameraPosition
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-        mMap.moveCamera(cameraUpdate);
+                        Log.d("DEBUG", "onMarkerClick()");
+
+                        View bottomSheet = findViewById(R.id.ap_bottom_sheet);
+                        mBottomSheetBehavior1 = BottomSheetBehavior.from(bottomSheet);
+
+                        if (mBottomSheetBehavior1.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                            Log.d("DEBUG", "expand bottom sheet");
+                            mBottomSheetBehavior1.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        } else {
+                            mBottomSheetBehavior1.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                            Log.d("DEBUG", "collapse bottom sheet");
+
+                        }
+
+                        return true;
+                    }
+                    return false;
+                }
+            });
 
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 
 
 
@@ -226,34 +289,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     // if access_fine_location is granted, automatically it is granted coarse_loc too
     public boolean checkLocationPermission() {
 
+        Log.d("DEBUG", "checkLocationPermission()");
         // for api level greater than 23 (marshmallow) - with runtime permissions, for api level
         // 22 and lower checking runtime permissions doesn't make sense
         // perché tanto se non si accetta tutto all'inizio l'app non viene installata
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            // il controllo posso spostarlo prima di chiamare la checkLocationPermission
-
-            // se il permesso è attualmente disabilitato
+            /* if permission is currently disabled */
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
 
-                // Should we show an explanation?
-                //mostriamo un dialog con le spiegazioni dovute
-                // shouldShow... ritorna true se precedentemente l'utente ha selezionato
-                // "nega permessi"
+                /* this method returns true if the user has previously selected "deny permission",
+                * in this case we show an explanation dialog */
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                    // Show an explanation to the user *asynchronously* -- don't block
-                    // this thread waiting for the user's response! After the user
-                    // sees the explanation, try again to request the permission.
+                    /* Show an explanation to the user *asynchronously*  After the user
+                     sees the explanation, try again to request the permission.*/
                     new AlertDialog.Builder(this)
                             .setTitle(R.string.title_location_permission)
                             .setMessage(R.string.text_location_permission)
                             .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    //Prompt the user once explanation has been shown
+
                                     ActivityCompat.requestPermissions(MainActivity.this,
                                             new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                                             MY_PERMISSIONS_REQUEST_LOCATION);
@@ -264,8 +323,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
                 } else {
-                    //vado qua se è la prima volta che richiedo il permesso
-                    //faccio la richiesta senza spiegazioni
+
+                    /* this is the first time requesting the permission, I ask without explanations */
                     ActivityCompat.requestPermissions(this,
                             new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                             MY_PERMISSIONS_REQUEST_LOCATION);
@@ -276,40 +335,40 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
 
         }
-        // significa che il permesso è stato accettato in fase di installazione (sono su una versione
-        //più vecchia)
+        /* means permission was granted during installation */
         else return true;
     }
 
-    // callback per gestire la risposta dell'utente alla richiesta
+    /* this callback handles the user answers to the location request */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
+
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
+                /* if request is cancelled, the result arrays are empty */
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
+                    /* permission was granted, so I can do the location-related task */
                     if (ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED) {
 
-                        //Request location updates:
-                        //locationManager.requestLocationUpdates(provider, 400, 1, this);
-                        if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER))
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                        if (mGoogleApiClient != null) //anche locationRequest dev'essere valido!
+                        {
+                            mMap.setMyLocationEnabled(true);
+                            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+                        }
 
                     }
 
                 } else {
 
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    /* permission denied, disabling the functionality that depends on this permission.
                     // TODO: l'app non visualizzerà più la propria posizione ma continuerà a funzionare
                     // avendo un database di ap e usando degli indirizzi per navigare la mappa
+                    */
                 }
                 return;
             }
